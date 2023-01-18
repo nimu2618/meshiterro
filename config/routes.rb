5cfd7dad88e71fafd85b15857f26e2facbd43048
nimu2618 <119933351+nimu2618@users.noreply.github.com>
2023-01-18 07:08:40 +0000

    18章まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root to:"homes#top"
   devise_for :users
+  get "/homes/about" =>"homes#about", as:"about"
 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
 
-  get 'homes/about' =>'homes#about', as:'about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :edit, :update]
 end
